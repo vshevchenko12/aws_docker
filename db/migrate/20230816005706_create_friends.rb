173f411cbbbd47ee2fd4ea8f222bc0f74b79fe2f
Vitalii Shevchenko <vshevchenko12@gmail.com>
2023-08-25 11:48:07 -0300

    formatted using rubocop

--- db/migrate/20230816005706_create_friends.rb
+++ db/migrate/20230816005706_create_friends.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateFriends < ActiveRecord::Migration[7.1]
   def change
     create_table :friends do |t|
--- db/migrate/20230816011720_add_user_id_to_friends.rb
+++ db/migrate/20230816011720_add_user_id_to_friends.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddUserIdToFriends < ActiveRecord::Migration[7.1]
   def change
     add_column :friends, :user_id, :integer
